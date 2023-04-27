2f9460258718ba95d6c73206ae735a58f5baa9f8
EnriqueLiGarcia <quiquegarcia0@gmail.com>
2023-04-27 19:23:53 +0200

    se agrega el proyecto

---
+++ src/PagG12/Hub.java
@@ -0,0 +1,98 @@
+package PagG12;
+public class Hub {
+    private Contenedor[][] m;
+    public Hub(){
+
+        this.m = new Contenedor[10][12];
+
+    }
+
+    public Contenedor[][] getC(){
+
+        return this.m;
+
+    }
+
+    public void setC(Contenedor[][] m){
+
+        if(m != null){
+
+            this.m=m;
+        }
+
+    }
+
+// ToString
+
+    public String toString(){
+
+        String s = new String();
+
+
+        for(int i=0 ; i<m.length ; i++){ //FILAS
+
+            for(int j=0 ; j<m[i].length ; j++){ //COLUMNAS de la fila i
+
+                if(m[i][j] == null) s += "[N]";
+                else s += "[" + m[i][j].getId() + "]";
+
+
+
+            }
+
+            s += "\n";
+        }
+
+
+        return s;
+
+
+
+    }
+
+    public Contenedor desapilar(int col){
+
+        if (col<0 || col>=m[0].length) return null; //m[0].length nos vale porque todas las columnas tienen la misma longitud
+
+        for(int i=0 ; i<m.length ; i++){
+
+            if(m[i][col] != null){
+
+                Contenedor aux = m[i][col];
+                m[i][col]=null; //Lo borro
+
+                return aux;
+
+            }
+
+        }
+
+        return null;
+
+    }
+
+    public void apilar(Contenedor c) {
+        if (c != null) {
+            int p = c.getPrioridad();
+            if (p == 1 || p == 2) {
+                int col = p - 1;
+                for (int i = m.length - 1; i >= 0; i--) {
+                    if (m[i][col] == null) {
+                        m[i][col] = c;
+                        return;
+                    }
+                }
+            }else if(p==3){
+                for (int i = m.length - 1; i >= 0; i--) {
+                    for (int j = 2; j < 12 ; j++) {
+                        if(m[i][j]==null){
+                            m[i][j]=c;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
